5065c6aac1e5ea3da5537f0a2dfa50707ea1a3f3
kyanysh <kyanyshbd30@gmail.com>
2023-04-27 15:21:24 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+// const obj = {               //приветствие 
+//     key: 'value',
+//     name: 'Ijamal',
+//     text: 'Goodmorning teacher'
+// }
+
+// console.log(obj.name, obj.text);
+
+// var num = 0;                       //Равенства
+// var obj = new String("0");
+// var str = "0";
+// var b = false;
+
+// console.log(num == num); 
+// console.log(obj == obj); 
+// console.log(str == str); 
+
+// console.log(num == obj);
+// console.log(num == str); 
+// console.log(obj == str); 
+// console.log(null == undefined); 
+
+// console.log(obj == null);
+// console.log(obj == undefined);
+
+// const colorList = [ //Светафор
+//     { name: 'yellow', interval: 500 },
+//     { name: 'red', interval: 4000 },
+//     { name: 'yellow', interval: 500 },
+//     { name: 'green', interval: 4000 },
+//     { name: 'you can cross the traffic light', interval: 100 },
+// ];
+
+// let count = 0;
+
+// function changeColor() {
+//     if (count === colorList.length) {
+//         count = 0;
+//     }
+//     console.log(colorList[count].name)
+//     setTimeout(changeColor, colorList[count].interval)
+//     count = count + 1;
+// }
+
+// changeColor()
